a19cc4f6490b1a6a82a374a69d8eb61d891ec5b7
may11224 <39835412+may1122@users.noreply.github.com>
2026-06-24 22:11:51 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("AYÇA Yazılım yayında.");
+const form = document.getElementById("demoForm");
+const message = document.getElementById("formMessage");
+
+form.addEventListener("submit", function(event){
+  event.preventDefault();
+
+  message.textContent = "Demo talebiniz alınmıştır. En kısa sürede sizinle iletişime geçeceğiz.";
+
+  form.reset();
+});
