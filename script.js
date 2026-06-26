651702029a591d0fa995f9eb6be69fa27b25a2d1
may11224 <39835412+may1122@users.noreply.github.com>
2026-06-26 09:19:45 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-document.querySelectorAll(‘a[href^=“#”]’).forEach(link => {
-link.addEventListener(“click”, function (e) { const target =
-document.querySelector(this.getAttribute(“href”));
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+  link.addEventListener("click", function (e) {
+    const target = document.querySelector(this.getAttribute("href"));
 
     if (target) {
       e.preventDefault();
@@ -9,13 +9,20 @@ document.querySelector(this.getAttribute(“href”));
         block: "start"
       });
     }
+  });
+});
 
-}); });
+const revealItems = document.querySelectorAll(".reveal");
 
-const revealItems = document.querySelectorAll(“.reveal”);
-
-const observer = new IntersectionObserver( entries => {
-entries.forEach(entry => { if (entry.isIntersecting) {
-entry.target.classList.add(“show”); } }); }, { threshold: 0.12 } );
+const observer = new IntersectionObserver(
+  entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("show");
+      }
+    });
+  },
+  { threshold: 0.12 }
+);
 
 revealItems.forEach(item => observer.observe(item));
